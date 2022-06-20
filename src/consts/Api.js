313b44c9f8580049888d6d96a4c14bd4ff2de7ac
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2022-06-20 14:11:11 -0500

    seventh commit

---
+++ src/consts/Api.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import {Alert} from 'react-native';
+
+const Post = async (url, data, token) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    const response = await axios.post(url, data, headers);
+    return response;
+  } catch (error) {
+    return Alert.alert(`${error.name}`, `${error.message}`, [{text: 'OK'}]);
+  }
+};
+
+const Put = async (url, data, token) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    const response = await axios.put(url, data, headers);
+    return response;
+  } catch (error) {
+    return Alert.alert(`${error.name}`, `${error.message}`, [{text: 'OK'}]);
+  }
+};
+
+const Get = async (url, data, token) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    const response = await axios.get(url, headers);
+    return response;
+  } catch (error) {
+    return Alert.alert(`${error.name}`, `${error.message}`, [{text: 'OK'}]);
+  }
+};
+
+export {Post, Put, Get};
---
+++ src/consts/Url.js
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Url = endpoint => {
+  const url = 'www.newchinahut.herokuapp.com';
+
+  return `${url}/endpoint`;
+};
+
+export default Url;
