3d3da0a0119529c89f19de88b2b82f95815f8838
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2022-06-20 14:12:57 -0500

    eight commit

--- src/consts/Url.js
+++ src/consts/Url.js
@@ -2,6 +2,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const Url = endpoint => {
+  // const url = 'localhost:3000';
   const url = 'www.newchinahut.herokuapp.com';
 
   return `${url}/endpoint`;
